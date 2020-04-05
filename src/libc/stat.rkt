35fc1ac5a8fe595a696d474377e88e8b7d00af64
David Wilson <davidwil@posteo.de>
2020-04-05 21:38:26 +0100

    Add filetype mask checking

--- src/libc/stat.rkt
+++ src/libc/stat.rkt
@@ -9,7 +9,11 @@
 
     (define ipath path)
     (define ifile-name file-name)
-    (define full-path (build-path path file-name))
+    
+    (define full-path
+      (if (string=? file-name "")
+          path
+          (build-path path file-name)))
  
     (define/public (get-stat)
       (c-stat 'get_stat))
@@ -28,6 +32,14 @@
     (define/public (get-modified-time) mtime)
     (define/public (get-created-time) ctime)
 
+    (define/public (get-is-regular-file?) (eq? is-regular-file? 1))
+    (define/public (get-is-directory?) (eq? is-directory? 1))
+    (define/public (get-is-character-device?) (eq? is-character-device? 1))
+    (define/public (get-is-block-device?) (eq? is-block-device? 1))
+    (define/public (get-is-fifo?) (eq? is-fifo? 1))
+    (define/public (get-is-symbolic-link?) (eq? is-symbolic-link? 1))
+    (define/public (get-is-socket?) (eq? is-socket? 1))
+    
     (define-inline-ffi c-stat #:compiler "clang"
       "#include <sys/stat.h>\n"
       "#include <sys/types.h>\n"
@@ -93,6 +105,34 @@
 
       "long get_ctime(void) {\n"
       "  return s.st_ctime;\n"
+      "}\n"
+
+      "int is_regular_file(void) {\n"
+      "  return S_ISREG(s.st_mode);\n"
+      "}\n"
+      
+      "int is_directory(void) {\n"
+      "  return S_ISDIR(s.st_mode);\n"
+      "}\n"
+
+      "int is_character_device(void) {\n"
+      " return S_ISCHR(s.st_mode);\n"
+      "}\n"
+
+      "int is_block_device(void) {\n"
+      " return S_ISBLK(s.st_mode);\n"
+      "}\n"
+
+      "int is_fifo(void) {\n"
+      "  return S_ISFIFO(s.st_mode);\n"
+      "}\n"
+
+      "int is_symbolic_link(void) {\n"
+      "  return S_ISLNK(s.st_mode);\n"
+      "}\n"
+
+      "int is_socket(void) {\n"
+      "  return S_ISSOCK(s.st_mode);\n"
       "}\n")
 
     (c-stat 'stat_for_file full-path)
@@ -109,6 +149,14 @@
     (define blocks (c-stat 'get_blocks))
     (define atime (c-stat 'get_atime))
     (define mtime (c-stat 'get_mtime))
-    (define ctime (c-stat 'get_ctime))))
+    (define ctime (c-stat 'get_ctime))
+
+    (define is-regular-file? (c-stat 'is_regular_file))
+    (define is-directory? (c-stat 'is_directory))
+    (define is-character-device? (c-stat 'is_character_device))
+    (define is-block-device? (c-stat 'is_block_device))
+    (define is-fifo? (c-stat 'is_fifo))
+    (define is-symbolic-link? (c-stat 'is_symbolic_link))
+    (define is-socket? (c-stat 'is_socket))))
 
 (provide stat%)
