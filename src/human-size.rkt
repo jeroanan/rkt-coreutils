b8458db5d280e0908d51f12b8cf9ea33c1e6443b
David Wilson <davidwil@posteo.de>
2020-04-08 21:41:25 +0100

    Move human readable size code into own file

---
+++ src/human-size.rkt
@@ -0,0 +1,36 @@
+#lang racket/base
+
+(define (get-human-readable-size byte-size unit-size unit-symbol)
+  (let* ([whole-things (floor (/ byte-size unit-size))]
+         [remainder (modulo byte-size unit-size)]
+         [remainder-decimal
+          (inexact->exact
+           (ceiling
+            (exact->inexact
+             (* 10 (/ remainder unit-size)))))]
+         [is-kilobytes? (eq? unit-size 1024)]
+         [omit-decimal? (or is-kilobytes? (eq? remainder-decimal 0))]
+         [whole-thing-string (number->string
+                              (if
+                               (and is-kilobytes? (> remainder-decimal 0))
+                               (+ whole-things 1)
+                               whole-things))]
+         [decimal-string (string-append "." (number->string remainder-decimal))]
+         [output (string-append
+                  whole-thing-string
+                  (if omit-decimal? "" decimal-string)
+                  unit-symbol)])
+    output))
+  
+(define (human-readable-byte-size byte-size)
+  (let* ([kilobyte 1024]
+         [megabyte (* kilobyte 1024)]
+         [gigabyte (* megabyte 1024)])
+    (cond
+      [(< byte-size kilobyte) (number->string byte-size)]
+      [(< byte-size megabyte) (get-human-readable-size byte-size kilobyte "K")]
+      [(and (>= byte-size megabyte) (< byte-size gigabyte))
+       (get-human-readable-size byte-size megabyte "M")] 
+      [else (get-human-readable-size byte-size gigabyte "G")])))
+
+(provide human-readable-byte-size)
--- src/ls.rkt
+++ src/ls.rkt
@@ -2,7 +2,8 @@
 
 (require "libc/stat.rkt"
          "libc/pwd.rkt"
-         "libc/grp.rkt")
+         "libc/grp.rkt"
+         "human-size.rkt")
 
 (define show-hidden (make-parameter #f))
 (define hide-implied (make-parameter #t))
@@ -55,39 +56,6 @@
   (cond
     [(send stat get-is-directory?) "d"]
     [else "-"]))
-
-(define (get-human-readable-size byte-size unit-size unit-symbol)
-  (let* ([whole-things (floor (/ byte-size unit-size))]
-         [remainder (modulo byte-size unit-size)]
-         [remainder-decimal
-          (inexact->exact
-           (ceiling
-            (exact->inexact
-             (* 10 (/ remainder unit-size)))))]
-         [is-kilobytes? (eq? unit-size 1024)]
-         [omit-decimal? (or is-kilobytes? (eq? remainder-decimal 0))]
-         [whole-thing-string (number->string
-                              (if
-                               (and is-kilobytes? (> remainder-decimal 0))
-                               (+ whole-things 1)
-                               whole-things))]
-         [decimal-string (string-append "." (number->string remainder-decimal))]
-         [output (string-append
-                  whole-thing-string
-                  (if omit-decimal? "" decimal-string)
-                  unit-symbol)])
-    output))
-  
-(define (human-readable-byte-size byte-size)
-  (let* ([kilobyte 1024]
-         [megabyte (* kilobyte 1024)]
-         [gigabyte (* megabyte 1024)])
-    (cond
-      [(< byte-size kilobyte) (number->string byte-size)]
-      [(< byte-size megabyte) (get-human-readable-size byte-size kilobyte "K")]
-      [(and (>= byte-size megabyte) (< byte-size gigabyte))
-       (get-human-readable-size byte-size megabyte "M")] 
-      [else (get-human-readable-size byte-size gigabyte "G")])))
 
 (define-syntax-rule (get-permissions-mode name rwx r w x)
   (define (name stat)
