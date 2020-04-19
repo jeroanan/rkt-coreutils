1672485f62f4cb4446e97ce3b6be34b51bf78253
David Wilson <davidwil@posteo.de>
2020-04-19 09:22:48 +0100

    add unistd.rkt

---
+++ src/libc/unistd.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+; Copyright 2020 David Wilson
+
+;This program is free software: you can redistribute it and/or modify
+;it under the terms of the GNU General Public License as published by
+;the Free Software Foundation, either version 3 of the License, or
+;(at your option) any later version.
+;
+;This program is distributed in the hope that it will be useful,
+;but WITHOUT ANY WARRANTY; without even the implied warranty of
+;MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;GNU General Public License for more details.
+;
+;You should have received a copy of the GNU General Public License
+;along with this program.  If not, see <https://www.gnu.org/licenses/>.
+(provide geteuid)
+
+(require ffi/unsafe)
+
+(define clib (ffi-lib #f))
+
+(define _geteuid (get-ffi-obj
+                      "geteuid" clib
+                      (_fun -> _int)))
+
+(define (geteuid)
+  (_geteuid))
+
