324127707c0e6d22c566fb4f58766d6fa761e9bd
David Wilson <davidwil@posteo.de>
2020-04-23 21:29:59 +0100

    sha programs now accept stdinput

--- src/repl/util/program/shaprogram.rkt
+++ src/repl/util/program/shaprogram.rkt
@@ -20,8 +20,27 @@
               (displayln hs))))
 
         (define/public (execute [files : (Listof String)])
+          (if (empty? files)
+              (process-stdin)
+              (process-files files)))
+
+        (: process-stdin (-> Void))
+        (define/private (process-stdin)
+          (let ([the-hash (port->hash-string (current-input-port))])
+            (display-result the-hash "-")))
+
+        (: process-files (-> (Listof String) Void))
+        (define/private (process-files files)
           (for ([file files])
             (let* ([f (open-input-file file #:mode 'text )]
-                   [the-hash (sha-method (port->bytes f))]
-                   [hash-string (bytes->hex-string the-hash)])
-              (displayln (format "~a ~a" hash-string file)))))))))
+                   [the-hash (port->hash-string f)])
+              (display-result the-hash file))))
+
+        (: port->hash-string (-> Input-Port String))
+        (define/private (port->hash-string ip)
+          (let ([the-hash (sha-method (port->bytes ip))])
+            (bytes->hex-string the-hash)))
+
+        (: display-result (-> String String Void))
+        (define/private (display-result hash-str file-name)
+          (displayln (format "~a  ~a" hash-str file-name)))))))
