22f3920aebaedb246a1c7e1283121d84364163f3
David Wilson <davidwil@posteo.de>
2020-04-27 23:11:08 +0100

    add logname

---
+++ src/logname.rkt
@@ -0,0 +1,9 @@
+#lang typed/racket/base
+
+; Copyright 2020 David Wilson
+; See COPYING for details
+
+(require "repl/logname.rkt"         
+         "util/simple-program.rkt")
+
+(simple-program logname%)
--- src/repl/logname.rkt
+++ src/repl/logname.rkt
@@ -1,1 +1,21 @@
-#lang racket
+#lang typed/racket/base
+
+; Copyright 2020 David Wilson
+; See COPYING for details
+
+(provide logname%)
+
+(require typed/racket/class)
+
+(require "util/program/getusernameprogram.rkt")
+
+(require/typed "../libc/unistd.rkt"
+               [get-uid (-> Integer)])
+
+(define help-strings (list "Print logged-in userid"
+                               ""
+                               "Methods:"
+                               "(help) -- display this help message"
+                               "(execute) -- display logged-in userid"))
+
+(get-user-name-program logname% help-strings get-uid)
