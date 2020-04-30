e8b068e58e2e5dc238b77e83ec62425865340f67
David Wilson <davidwil@posteo.de>
2020-04-30 18:54:03 +0100

    Revert

--- src/repl/util/line-by-line-processor.rkt
+++ src/repl/util/line-by-line-processor.rkt
@@ -9,35 +9,21 @@
          racket/format
          racket/list)
 
-(define-syntax (line-by-line-processor line-function)
-  (syntax-rules ()
-    [(line-by-line-processor line-function)
-     (begin 
-       (define/public (execute [files : (Listof String)])
-         (begin
-           (if (empty? files)
-               (process-stdin)
-               (process-files files))))
+(define-syntax-rule (line-by-line-processor line-function)
+  (begin 
+    (define/public (execute [files : (Listof String)])
+      (begin
+        (if (empty? files)
+            (process-stdin)
+            (process-files files))))
 
-       (_process-files)
+    (: process-files (-> (Listof String) Void))
+    (define/private (process-files files)
+      (for ([file-name files])
+        (let* ([f (open-input-file file-name #:mode 'text )])
+          (for ([l (in-lines f)])
+            (line-function l)))))
 
-       (_process-stdin))]
-    [(line-by-line-processor line-function on-before-processing-function)
-     (begin
-       (define/public (execute [files : (Listof String)])
-         (begin
-           (on-before-processing-function)
-           (if (empty? files)
-               (process-stdin)
-               (process-files files))))
-
-       (_process-files)
-
-       (_process-stdin))]))
-    
-
-(define-syntax-rule (_process-stdin)
-  (begin
     (: process-stdin (-> Void))
     (define/private (process-stdin)
       (let* ([r (read-line)]
@@ -45,12 +31,3 @@
         (when (not (eof-object? r))
           (line-function rs)
           (process-stdin))))))
-
-(define-syntax-rule (_process-files)
-  (begin
-    (: process-files (-> (Listof String) Void))
-    (define/private (process-files files)
-      (for ([file-name files])
-        (let* ([f (open-input-file file-name #:mode 'text )])
-          (for ([l (in-lines f)])
-            (line-function l)))))))
