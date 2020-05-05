37323b3a486de60d9e1f2d03071b9e61ed24bd1e
David Wilson <davidwil@posteo.de>
2020-05-05 20:49:25 +0100

    uptime WIP

---
+++ src/repl/uptime.rkt
@@ -0,0 +1,77 @@
+#lang typed/racket/base
+
+;; Copyright 2020 David Wilson
+;; See COPYING for licence
+
+(provide uptime%)
+
+(require typed/racket/class
+         typed/racket/date
+         racket/format
+         racket/list
+         racket/port
+         racket/string)
+
+(require "util/util.rkt")
+
+;; The file in GNU/Linux that holds the uptime number.
+(define uptime-file "/proc/uptime")
+
+(define seconds-per-day 86400)
+(define seconds-per-hour 3600)
+(define seconds-per-minute 60)
+
+;; Uptime: Get system uptime
+(define uptime%
+  (class object%
+    (super-new)
+
+    (help-function (list "Display system uptime"
+                         ""
+                         "Methods:"
+                         "(help) -- Display this help message"
+                         "(execute) -- Display system uptime"))
+    
+    ;; Main program exectuion
+    (define/public (execute)
+      (let* ([f (open-input-file uptime-file #:mode 'text)]
+             [c (first (port->lines f))]
+             [secs (assert (string->number (first (string-split c " "))) number?)]
+             [uptime-days (floor (assert (/ secs seconds-per-day) real?))]
+             [uptime-days-secs (* uptime-days seconds-per-day)]
+             [secs-less-days (- secs uptime-days-secs)]
+             [uptime-hours  (floor (assert (/ secs-less-days seconds-per-hour) real?))]
+             [uptime-hours-secs (* uptime-hours 3600)]
+             [secs-less-days-and-hours (- secs uptime-days-secs uptime-hours-secs)]
+             [uptime-minutes (floor (assert (/ secs-less-days-and-hours seconds-per-minute) real?))])
+        (format "~a up ~a days, ~a:~a"
+                (current-time)
+                (real->integer-string uptime-days)
+                (real->integer-string uptime-hours)
+                (real->integer-string uptime-minutes))))
+
+    ;; Take a real number (e.g. 39.0) and return the integer portion (e.g. 39) as a string
+    (: real->integer-string (-> Real String))
+    (define/private (real->integer-string real-in)
+      (let* ([s (number->string real-in)]
+             [sp (string-split s ".")])
+        (first sp)))
+
+    (: current-time (-> String))
+    (define/private (current-time)
+      (let* ([d (current-date)]
+             [h (date-hour d)]
+             [m (date-minute d)]
+             [s (date-second d)])
+        (format "~a:~a:~a" (pad-zero h) (pad-zero m) (pad-zero s))))
+
+    (: pad-zero (-> Integer String))
+    (define/private (pad-zero num-in)
+      (let ([s (number->string num-in)])
+        (if (< num-in 10) 
+            (format "0~a" s)
+            s)))))
+
+    
+
+    
