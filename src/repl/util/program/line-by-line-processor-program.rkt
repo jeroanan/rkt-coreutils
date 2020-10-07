ad3c43e15daa993bcb4923bd395bf4cbf134f5fe
David Wilson <davidwil@posteo.de>
2020-10-07 20:42:38 +0100

    get rid of old line-by-line-processor-program

--- src/repl/util/program/line-by-line-processor-program.rkt
+++ src/repl/util/program/line-by-line-processor-program.rkt
@@ -4,59 +4,12 @@
 ; See COPYING for details
 
 (provide (all-from-out "repl-program.rkt") 
-         line-by-line-processor-program
-         line-by-line-processor
-         process-line-by-line
-         attribute)
+         line-by-line-processor)
 
 (require (for-syntax  racket/list)
          (for-syntax racket/base)
          racket/format
          racket/list)
-
-(require "../attribute.rkt")
-
-(define-syntax (line-by-line-processor-program stx)    
-  (syntax-case stx ()
-    [(_ type-name help-text line-handler-body finished-body extras ...)    
-   
-     #'(begin
-         (provide type-name)
-         (require "util/help.rkt")
-         (define type-name
-           (class object%
-             (super-new)
-
-             (help-function help-text)
-
-             extras ...
-              
-             (define (output-function x)
-               (line-handler-body x))
-
-             (define (finished)
-               (unless (null? finished-body)
-                 (finished-body)))
-
-             (on-execute-with-strings files
-                                      (begin
-                                        (if (empty? files)
-                                            (process-stdin)
-                                            (process-files files))))
-
-             (define/private (process-files files)
-               (for ([file-name files])
-                 (let* ([f (open-input-file file-name #:mode 'text )])
-                   (for ([l (in-lines f)])
-                     (output-function l))
-                   (finished))))
-
-             (define/private (process-stdin)
-               (let* ([r (read-line)]
-                      [rs (~a r)])
-                 (when (not (eof-object? r))
-                   (output-function rs)
-                   (process-stdin)))))))]))
 
 (define-syntax-rule (line-by-line-processor name line-processor eof-processor)
   (begin
