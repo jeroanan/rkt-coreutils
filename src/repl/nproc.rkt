b5801b8dfef54f5d8f49baa5a03b4a88d496baf7
David Wilson <davidwil@posteo.de>
2021-08-22 09:38:49 +0100

    Make nproc use racket/base
    Remove a lot of commented-out code.

--- src/repl/nproc.rkt
+++ src/repl/nproc.rkt
@@ -1,24 +1,12 @@
-#lang s-exp "util/program/repl-program.rkt"
+#lang racket/base ;s-exp "util/program/repl-program.rkt"
 
-; Copyright 2020 David Wilson
+; Copyright 2020, 2021 David Wilson
 ; See COPYING for details
 
 (provide nproc)
 
 (require nproc)
 
-    #;(help-function "Print number of procesing units"
-                   (list "(execute) - Print number of processing units"))
 (define nproc
   (λ ()
     (displayln (get-number-of-processors))))
-;; nproc - Print number of processing units
-#;(define nproc%
-  (class object%
-    (super-new)
-
-    (help-function "Print number of procesing units"
-                   (list "(execute) - Print number of processing units"))
-
-    (on-execute-with-void 
-      (displayln (get-number-of-processors)))))
